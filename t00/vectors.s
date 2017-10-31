11f538ebb59fff057b0f7e8901418d0a34151d90
dwelch <dwelch@dwelch.com>
2017-10-30 22:25:22 -0400

    wip

--- t00/vectors.s
+++ t00/vectors.s
@@ -4,13 +4,9 @@
 _start:
 .word 0x20001000
 .word reset
-.word three
-.word four
+.word done
 .thumb_func
 reset:
-    b three
-three:
-    b three
-.thumb_func
-four:
-    b three
+    b done
+done:
+    b done
